9b8cbbe0cecb9a45f9b504f2a4697d20a0f8d894
Francisco Velazco <36135792+fjvelazcop@users.noreply.github.com>
2025-04-18 19:46:45 -0400

    Add frontend/src/app/services/auth.service.ts 1745020005649

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:4000/api/auth';
+  private tokenKey = 'auth_token';
+  private loggedIn = new BehaviorSubject<boolean>(this.hasToken());
+
+  constructor(private http: HttpClient) {}
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem(this.tokenKey);
+  }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { email, password })
+      .pipe(
+        tap(response => {
+          localStorage.setItem(this.tokenKey, response.token);
+          this.loggedIn.next(true);
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.loggedIn.next(false);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+}
