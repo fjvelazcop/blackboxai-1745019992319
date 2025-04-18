8d07439714f3ad6aeab982178c8d83aa2996bf71
Francisco Velazco <36135792+fjvelazcop@users.noreply.github.com>
2025-04-18 19:46:48 -0400

    Add frontend/src/app/app.module.ts 1745020008242

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DoctorListComponent } from './components/doctor-list/doctor-list.component';
+import { DoctorFormComponent } from './components/doctor-form/doctor-form.component';
+import { MedicalRecordListComponent } from './components/medical-record-list/medical-record-list.component';
+import { MedicalRecordFormComponent } from './components/medical-record-form/medical-record-form.component';
+import { AuthInterceptor } from './services/auth.interceptor';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DoctorListComponent,
+    DoctorFormComponent,
+    MedicalRecordListComponent,
+    MedicalRecordFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      isolate: false
+    })
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
