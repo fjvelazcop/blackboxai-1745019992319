dbe57ed91f9305074161cb985ac5f279b6a49e4f
Francisco Velazco <36135792+fjvelazcop@users.noreply.github.com>
2025-07-18 13:08:03 -0400

    Add backend/src/routes/index.js 1752858487143

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./auth');
-const userRoutes = require('./users');
 const doctorRoutes = require('./doctors');
 const medicalRecordRoutes = require('./medicalRecords');
 
 router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
 router.use('/doctors', doctorRoutes);
 router.use('/medical-records', medicalRecordRoutes);
 
